aff98989dbd489c10f4ca45e8732133208fe8b9b
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2021-12-09 14:11:40 +0530

    build the js file

--- main.js
+++ main.js
@@ -31,14 +31,120 @@ function convertDateToString(date)
     else{
         dateStr.month=date.month.toString();
     }
-    if(date.year <10)
-    {
-        dateStr.year=date.year +'0';
-    }
-    else{
-        dateStr.year=date.year.toString();
-    }
     
+    dateStr.year=date.year.toString();
+    return dateStr;
+    
+
+}
+
+function getAllDateFormats(date)
+{
+    var dateStr = convertDateToStr(date);
+    var ddmmyyyy= dateStr.day + dateStr.month + dateStr.year;
+    var mmddyyyy=dateStr.month+ dateStr.day+ dateStr.year;
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+    var ddmmyy= dateStr.day+dateStr.month+
+        dateStr.year.slice(-2);
+    var mmddyy=dateStr.month+dateStr.day+
+        dateStr.year.slice(-2);
+    var yymmdd=dateStr.year.slice(-2)+dateStr.month+dateStr.day;
+
+    return [ddmmyyyy,mmddyyyy,yyyymmdd , ddmmyy, mmddyy , yymmdd];
 
 
 }
+function checkPalindromeForAllDateFormats(date)
+{
+    var listOfPalindromes=getAllDateFormats(date);
+    var flag=false;
+    for( var i=0;i<listOfPalindromes.length;i++)
+    {
+        if(isPalindrome(listOfPalindromes[i])){
+            flag=true;
+            break;
+        }
+    }
+    return flag;
+}
+function isLeapYear(year)
+{
+    if(year % 400===0)
+    {
+        return true;
+
+    }
+    if(year%100===0)
+    {
+        return false;
+    }
+    if(year %4===0)
+    {
+        return true;
+    }
+    return false;
+}
+function getNextDate() 
+{
+    var day=date.day +1;
+    var month= date.month;
+    var year = date.year;
+    var daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+    if(month===2)
+    {
+        if(isLeapYear(year))
+        {
+            if(day>29)
+            {
+                day=1;
+                month++;
+            }
+        }
+        else{
+            if(day>28)
+            {
+                day=1;
+                month++;
+            }
+        }
+
+    }
+    else{
+        if(day> daysInMonth[month-1 ])
+        {
+            day=1;
+            month++;
+        }
+    }
+    if(month>12)
+    {
+        month=1;
+        year++;
+    }
+    return {
+        day:day,
+        month:month,
+        year:year
+    };
+
+}
+function getNextPalindromeDate(date)
+{
+    var ctr =0;
+    var nextDate = getNextDate(date);
+
+    while(1)
+    {
+        ctr++;
+        var isPalindrome = checkPalindromeForAllDateFormats(nextDate);
+        if(isPalindrome)
+        {
+            break;
+        }
+        nextDate = getNextDate(nextDate);
+    }
+    return [ctr,nextDate];
+}
+
+    
+
